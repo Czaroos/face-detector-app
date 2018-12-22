13b07352abc1151f459d9e94780f5e6b447df3f0
Czarek <czaroosik@gmail.com>
2018-12-22 15:34:24 +0100

    made detect button with arrow icon

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Tilt from 'react-tilt';
+import GoButton from './go.svg';
 
 const ImageLinkForm = () => {
   return (
@@ -6,10 +8,16 @@ const ImageLinkForm = () => {
       <p className='tc f3 near-white'>
         {'This will detect faces in your picture. Give it a try.'}
       </p>
-      <div className='center h2'>
-        <button className='f5 link ba br2 b--silver bg-transparent near-white grow mr3' type='text'>Paste URL</button>
-        <input className='f6 w-50 ba br3 b--silver bg-transparent near-white' type='text' />
-        <button className='f3 ph3 link ba br2 b--white-10 bg-transparent near-white ml3 grow'>Detect</button>
+      <div className='center'>
+        <button className='mt3 h2 f5 link ba br2 b--silver bg-transparent near-white grow mr3' type='text'>Paste URL</button>
+        <input className='mt3 h2 f6 w-50 ba br3 b--silver bg-transparent near-white' type='text' />
+        <div className='ml3 relative  dim pointer'>
+          <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 64, width: 64 }} >
+            <div className="Tilt-inner">
+              <img src={GoButton} alt='Go'/>
+            </div>
+          </Tilt>
+        </div>
       </div>
     </div>
   );
