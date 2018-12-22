bd87e66d83669e5a3de1c3f57ed99bcc970b9795
Czarek <czaroosik@gmail.com>
2018-12-22 16:26:15 +0100

    Added react-particles-js to background

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ const ImageLinkForm = () => {
       <div className='center'>
         <button className='mt3 h2 f5 link ba br2 b--silver bg-transparent near-white grow mr3' type='text'>Paste URL</button>
         <input className='mt3 h2 f6 w-50 ba br3 b--silver bg-transparent near-white' type='text' />
-        <div className='ml3 relative  dim pointer'>
+        <div className='ml3 dim pointer'>
           <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 64, width: 64 }} >
             <div className="Tilt-inner">
               <img src={GoButton} alt='Go'/>
