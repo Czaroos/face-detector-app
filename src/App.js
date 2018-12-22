68463caed5065569f223011b240efaf5699774df
Czarek <czaroosik@gmail.com>
2018-12-22 22:32:13 +0100

    Implemented image face recognition with clarifai API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+
+const app = new Clarifai.App({
+  apiKey: 'a2dff173ba794df394bdbdf2372b94d4'
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +26,46 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: '',
+      box: {}
+    }
+  }
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  };
+}
+
+displayFaceBox = (box) => {
+  this.setState({box: box})
+}
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value})
+}
+
+onSubmit = () => {
+  document.getElementById('imageUrlInput').value = '';
+  this.setState({imageURL: this.state.input});
+  app.models.predict(
+  Clarifai.FACE_DETECT_MODEL,
+  this.state.input)
+  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err => console.log(err));
+}
+
   render() {
     return (
       <div className="App">
@@ -27,8 +74,12 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm
+        onInputChange={this.onInputChange}
+        onSubmit={this.onSubmit}/>
+        <FaceRecognition
+        box={this.state.box}
+        imageURL={this.state.imageURL}/>
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Tilt from 'react-tilt';
 import GoButton from './go.svg';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className='tc f3 near-white'>
         {'This will detect faces in your picture. Give it a try.'}
       </p>
       <div className='center'>
-        <button className='mt3 h2 f5 link ba br2 b--silver bg-transparent near-white grow mr3' type='text'>Paste URL</button>
-        <input className='mt3 h2 f6 w-50 ba br3 b--silver bg-transparent near-white' type='text' />
-        <div className='ml3 dim pointer'>
+        <button className='mt3 h2 f6 f5-m link ba br2 b--silver bg-transparent near-white grow mr3' type='text'>Paste URL</button>
+        <input id="imageUrlInput" className='mt3 h2 f6 w-50 pl2 ba br3 b--silver bg-transparent near-white' type='text' onChange={onInputChange}/>
+        <div className='ml3 dim pointer' onClick={onSubmit}>
           <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 64, width: 64 }} >
             <div className="Tilt-inner">
               <img src={GoButton} alt='Go'/>
