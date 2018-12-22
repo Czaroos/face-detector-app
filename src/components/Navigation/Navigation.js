9d89ed2fce796745c73f8659e08a891378e666a8
Czarek <czaroosik@gmail.com>
2018-12-22 12:06:30 +0100

    Sign Out button styling & bg color set.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
+const Navigation = () => {
+  return (
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <p className='f3 link dim white-70 pa3 pointer'>Sign Out</p>
+    </nav>
+  );
+}
+
 export default Navigation;
