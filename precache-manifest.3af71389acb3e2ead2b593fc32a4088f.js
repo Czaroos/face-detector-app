42cf4efc3e50b0eec4e7b4914e7dfb8d98166a80
Czarek <czaroosik@gmail.com>
2018-12-23 11:55:26 +0100

    Updates

---
+++ precache-manifest.3af71389acb3e2ead2b593fc32a4088f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d9ab0669e552b158491f2852a8f3e76",
+    "url": "/face-detector-app/static/media/go.5d9ab066.svg"
+  },
+  {
+    "revision": "25850e8b36a0a04b08b2a1c2f10e9cd4",
+    "url": "/face-detector-app/static/media/face-detection.25850e8b.svg"
+  },
+  {
+    "revision": "4848d6ff7b5fd71f9973",
+    "url": "/face-detector-app/static/js/runtime~main.4848d6ff.js"
+  },
+  {
+    "revision": "71f09feac9be2305619b",
+    "url": "/face-detector-app/static/js/main.71f09fea.chunk.js"
+  },
+  {
+    "revision": "83801d4e47fe875d9f4f",
+    "url": "/face-detector-app/static/js/1.83801d4e.chunk.js"
+  },
+  {
+    "revision": "71f09feac9be2305619b",
+    "url": "/face-detector-app/static/css/main.ecd57e31.chunk.css"
+  },
+  {
+    "revision": "83801d4e47fe875d9f4f",
+    "url": "/face-detector-app/static/css/1.3436d986.chunk.css"
+  },
+  {
+    "revision": "6eda5ed35ec7d78774dbb13258a569e4",
+    "url": "/face-detector-app/index.html"
+  }
+];
